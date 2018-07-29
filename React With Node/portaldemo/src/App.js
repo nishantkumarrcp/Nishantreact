7796301e84cd3919562caf0df4b83f3f7079d2bd
kamalkrishna3000 <kamalkrishna3000@gmail.com>
2018-07-29 16:43:58 +0530

    Added Routing.

--- React With Node/portaldemo/src/App.js
+++ React With Node/portaldemo/src/App.js
@@ -2,14 +2,37 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import { Route, Switch } from 'react-router-dom';
+
 import RegistrationScreen from './containers/registration';
 import StudentScreen from './containers/student/list';
 import HomeScreen from './containers/home';
 
+class Login extends Component {
+  render() { 
+    return (
+      <div> 
+        Welcome to Login 
+        <input type="button" value="Login" onClick={ event => this.props.history.push('home')} />
+
+      </div> 
+    ) 
+  }
+}
+
+class UnImplemented extends Component {
+  render() { return (<div> Need to Implement</div> ) }
+}
+
 class App extends Component {
   render() {
     return (
-      <HomeScreen />
+      <Switch>
+        <Route path="/" exact component={Login} />
+        <Route path="/home" component={HomeScreen} />
+        <Route path="/register" component={RegistrationScreen} />
+        <Route path="/*" component={UnImplemented} />
+      </Switch>
     );
   }
 }
